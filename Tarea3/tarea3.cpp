b50de7513416ebdd8af6fea2e2be20dbceb67b20
SantivqzV <a01177850@tec.mx>
2023-09-01 14:51:52 -0600

    Se cambiaron los métodos a un ".h" file

--- Tarea3/tarea3.cpp
+++ Tarea3/tarea3.cpp
@@ -14,31 +14,6 @@
 #include "tarea3.h"
 using namespace std;
 
-/**
- * Convertir un IP a un valor en base 10
- * 
- * Para toda IP entra un vector temporal de 4 elementos, el primer elemento corresponde 
- *  al primer octeto de una IP y el ultimo corresponde al ultimo elemento de la IP. La
- *  formula general para la conversión es (valor de octeto 1 * 256^3) + (Valor de octeto 2 * 256^2) +
- *  (valor de octeto 3 * 256) + valor de octeto 4. Se procesa esta formula en la función y el resultado 
- *  se agrega a un vector que contiene los valores de la ip ya convertidos en base 10.
- * 
- * @param[in] vector<double> temp, vector que contiene los octetos de una dirección IP
- * @param[in, out] vector<double> ipsNuevo, vector que guarda los valores en base 10 de la IP.
- * 
- * @return vector<double> ipsNuevo, con un valor en base 10 nuevo de la IP.
- * 
- * @note complejidad de tiempo: O(n)
- * @note complejidad de espacio: O(1)
-*/
-void operacionConvertir(vector<double> &ipsNuevo, vector<double> &temp){
-    double total;
-    for(int i = 0; i <= 3; i++){
-        total += temp[3-i] * (pow(256, i));
-    }
-    ipsNuevo.push_back(total);
-}
-
 int main(){
     ///Definir vectores para guardar los IPs orginiales y los que serán convertidos
     vector<string> ips = {"0.0.0.1","0.1.0.1","1.1.1.1"};
--- Tarea3/tarea3.h
+++ Tarea3/tarea3.h
@@ -7,9 +7,33 @@
  * Fecha de modificación: 01/09/2023
  * V1
  * 
- * Programa donde dado n numeros de entrada que son almacenados en un vector<double>
- *  realizar las siguientes funciones de ordenamiento: Intercambio, Burbuja y Merge. 
- *  Y busqueda: secuencial y Binaria
 */
 #include <iostream>
+#include <vector>
+#include <math.h>
 using namespace std;
+
+/**
+ * Convertir un IP a un valor en base 10
+ * 
+ * Para toda IP entra un vector temporal de 4 elementos, el primer elemento corresponde 
+ *  al primer octeto de una IP y el ultimo corresponde al ultimo elemento de la IP. La
+ *  formula general para la conversión es (valor de octeto 1 * 256^3) + (Valor de octeto 2 * 256^2) +
+ *  (valor de octeto 3 * 256) + valor de octeto 4. Se procesa esta formula en la función y el resultado 
+ *  se agrega a un vector que contiene los valores de la ip ya convertidos en base 10.
+ * 
+ * @param[in] vector<double> temp, vector que contiene los octetos de una dirección IP
+ * @param[in, out] vector<double> ipsNuevo, vector que guarda los valores en base 10 de la IP.
+ * 
+ * @return vector<double> ipsNuevo, con un valor en base 10 nuevo de la IP.
+ * 
+ * @note complejidad de tiempo: O(n)
+ * @note complejidad de espacio: O(1)
+*/
+void operacionConvertir(vector<double> &ipsNuevo, vector<double> &temp){
+    double total;
+    for(int i = 0; i <= 3; i++){
+        total += temp[3-i] * (pow(256, i));
+    }
+    ipsNuevo.push_back(total);
+}
